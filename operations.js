51a130c3fac1be829e97386bb4a749613c26cbca
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-08-06 01:34:54 +0300

    Finished Image load/Finished Input conditions

--- operations.js
+++ operations.js
@@ -1,37 +1,75 @@
+
+var loadFile = function(event) {
+    var reader = new FileReader();
+    reader.onload = function(){
+      var output = document.getElementById('output');
+      output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
 function addRow() {
-          
+
+
+    /*
+        //--load image
+        var loadFile = function(event) {
+            var reader = new FileReader();
+            reader.onload = function(){
+              var output = document.getElementById('output');
+              output.src = reader.result;
+            };
+            reader.readAsDataURL(event.target.files[0]);
+          };
+          //-load image
+          */
     var nume = document.getElementById("nume");
     var prenume = document.getElementById("prenume");
     var email = document.getElementById("email");
     var data = document.getElementById("date");
     var img_path = document.getElementById("img_path");
-    
+
     var select = document.getElementById('sex');
     var sex_value = select.options[select.selectedIndex].value;
    // var sex = document.getElementById("sex");
-
-    if(document.getElementById("nume").value.length == 0 ||
-    document.getElementById("prenume").value.length == 0 ||
-    document.getElementById("email").value.length == 0 )
+/*
+   var elements = document.getElementsByTagName("INPUT");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].oninvalid = function(e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("This field cannot be left blank");
+        }
+    };
+    elements[i].oninput = function(e) {
+        e.target.setCustomValidity("");
+    };
+}
+*/
+    
    //  sex_value.has("option").length <= 0 )
 
    //if(!data.value)
     //data!=null ||    
    // document.getElementById("img_path").value.length == 0||
-
+   if(document.getElementById("nume").value.length == 0 ||
+   document.getElementById("prenume").value.length == 0 ||
+   document.getElementById("email").value.length == 0 ||
+   document.getElementById("date").value=="2028-04-04"||
+   document.getElementById("output").src=="http://127.0.0.1:5500/nullSource"||
+   sex_value=="")
   {
     alert("Complete all fields!");
     console.log(data.value);
 }
    
     else{
-        
+        console.log(sex_value);
         var table = document.getElementById("myTableData");
  
         var rowCount = table.rows.length;
         var row = table.insertRow(rowCount);
      
-        row.insertCell(0).innerHTML= "test";
+        row.insertCell(0).innerHTML= '<td align="center"><img src=' + document.getElementById("output").src + ' style="width:50px;border-radius: 50%;"></td>';
         row.insertCell(1).innerHTML= nume.value;
         row.insertCell(2).innerHTML= prenume.value;
         row.insertCell(3).innerHTML= email.value;
@@ -44,7 +82,7 @@ function addRow() {
 
  
 }
- 
+
 function deleteRow(obj) {
       
     var index = obj.parentNode.parentNode.rowIndex;
@@ -58,3 +96,4 @@ window.onbeforeunload = function() {
     return ("dsdsdS");
 }
 */
+
